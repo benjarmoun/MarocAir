c998c3f6af81b873fcd4289d1e116090f10c3958
benjarmoun <93975590+benjarmoun@users.noreply.github.com>
2022-10-17 20:02:05 +0100

    added user login

--- src/main/java/mvc/DAO/UserDAO.java
+++ src/main/java/mvc/DAO/UserDAO.java
@@ -12,6 +12,22 @@ import java.sql.ResultSet;
 import static mvc.helpers.Connexion.getConnection;
 
 public class UserDAO implements Auth<User> {
+    @Override
+    public boolean login(User user) throws Exception {
+        Connection connect = getConnection();
+        PreparedStatement stmt = connect.prepareStatement("select * from user where email = ?");
+        stmt.setString(1, user.getEmail());
+        ResultSet rs = stmt.executeQuery();
+
+        if (rs.next()) {
+
+            if (Hash.MD5(user.getPassword()).equals(rs.getString("password")))
+                return true;
+        }
+        return false;
+
+    }
+
 
 
 }
