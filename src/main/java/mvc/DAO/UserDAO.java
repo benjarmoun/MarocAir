fcc4d691e232331d1a33bd281af6473f76f3cbdb
benjarmoun <93975590+benjarmoun@users.noreply.github.com>
2022-10-18 11:16:46 +0100

    update login

--- src/main/java/mvc/DAO/UserDAO.java
+++ src/main/java/mvc/DAO/UserDAO.java
@@ -13,18 +13,24 @@ import static mvc.helpers.Connexion.getConnection;
 
 public class UserDAO implements Auth<User> {
     @Override
-    public boolean login(User user) throws Exception {
+    public User login(User user) throws Exception {
         Connection connect = getConnection();
         PreparedStatement stmt = connect.prepareStatement("select * from user where email = ?");
         stmt.setString(1, user.getEmail());
         ResultSet rs = stmt.executeQuery();
 
+                User user1 = new User();
         if (rs.next()) {
 
-            if (Hash.MD5(user.getPassword()).equals(rs.getString("password")))
-                return true;
+            if (Hash.MD5(user.getPassword()).equals(rs.getString("password"))){
+                user1.setId(rs.getInt("id"));
+                user1.setEmail(rs.getString("email"));
+                user1.setFname(rs.getString("fname"));
+                user1.setLname(rs.getString("lname"));
+            }
+                return user1;
         }
-        return false;
+        return null;
 
     }
 
